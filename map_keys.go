0fcbde014a3be2cfcdebaaffbe1432823de2c1e9
apeipo <apeipo@gmail.com>
2018-10-17 16:16:41 +0800

    feat(mapkeys): add SafeMapKeys

--- map_keys.go
+++ map_keys.go
@@ -2,6 +2,7 @@ package maputil
 
 import (
 	"reflect"
+	"fmt"
 )
 
 // return map keys as interface
@@ -15,4 +16,21 @@ func MapKeys(m interface{}) interface{} {
 		rkeys = reflect.Append(rkeys, key)
 	}
 	return rkeys.Interface()
+}
+
+func SafeMapKeys(m interface{}) (r interface{}, err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			r = nil
+			err = fmt.Errorf("%s", p)
+		}
+	}()
+	mtyp := reflect.TypeOf(m)
+	v := reflect.ValueOf(m)
+	keys := v.MapKeys() // panic if not map type
+	rkeys := reflect.MakeSlice(reflect.SliceOf(mtyp.Key()), 0, v.Len())
+	for _, key := range keys {
+		rkeys = reflect.Append(rkeys, key)
+	}
+	return rkeys.Interface(), nil
 }
--- map_keys_test.go
+++ map_keys_test.go
@@ -21,3 +21,22 @@ func TestMapKeysBasic(t *testing.T) {
 		assert.Assert(t, ok)
 	}
 }
+
+func TestSafeMapKeys(t *testing.T) {
+	m := map[string]rune {
+		"A" : 'A',
+		"B" : 'B',
+		"C" : 'C',
+	}
+
+	keys, err := SafeMapKeys(m)
+	assert.NilError(t, err)
+	realKeys, ok := keys.([]string)
+	assert.Assert(t, ok)
+	assert.Assert(t, len(realKeys) == len(m))
+
+	//not a map type
+	keys, err = SafeMapKeys("")
+	assert.Assert(t, err != nil)
+	t.Logf("%s", err)
+}
